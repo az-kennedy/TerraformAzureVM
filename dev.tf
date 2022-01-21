83f5782bf64eff9cd493c25f947a3df96acee334
Thomas Kennedy <thomas_kennedy@goodyear.com>
2022-01-21 07:20:16 -0500

    Configured Network Interface

--- dev.tf
+++ dev.tf
@@ -38,31 +38,6 @@ module "network" {
   depends_on = [azurerm_resource_group.dev]
 }
 
-output "vnet_id" {
-  description = "The id of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.id
-}
-
-output "vnet_name" {
-  description = "The name of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.name
-}
-
-output "vnet_location" {
-  description = "The location of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.location
-}
-
-output "vnet_address_space" {
-  description = "The address space of the newly created vNet"
-  value       = azurerm_virtual_network.vnet.address_space
-}
-
-output "vnet_subnets" {
-  description = "The ids of subnets created inside the newly created vNet"
-  value       = azurerm_subnet.subnet.*.id
-}
-
 # Build the VMs
 resource "azurerm_public_ip" "dev" {
    name                         = "publicIPForLB"
@@ -84,7 +59,7 @@ resource "azurerm_network_interface" "dev" {
 
   ip_configuration {
     name                          = "testConfiguration"
-    subnet_id                     = azurerm_subnet.subnet.1.id
+    subnet_id                     = module.network.vnet_subnets.1
     private_ip_address_allocation = "dynamic"
   }
 
@@ -94,6 +69,10 @@ resource "azurerm_network_interface" "dev" {
   }
 }
 
+output "SubNetID" {
+  value = module.network.vnet_subnets
+}
+/*
 resource "azurerm_managed_disk" "dev" {
   count                = 2
   name                 = "datadisk_existing_${count.index}"
@@ -171,3 +150,6 @@ resource "azurerm_virtual_machine" "dev" {
     "Environment" : "dev"
   }
 }
+*/
+
+# Outputs
