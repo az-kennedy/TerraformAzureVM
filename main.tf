5ac37655cc1ad29111408d593631e19a115b9c98
Thomas Kennedy <thomas_kennedy@goodyear.com>
2022-01-22 18:48:28 -0500

    began modulizing

---
+++ main.tf
@@ -0,0 +1,45 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.92.0"
+    }
+  }
+}
+
+# Setup
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-AzureVMs"
+  location = "eastus"
+
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+# Build out the network
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.dev.name
+  address_spaces      = ["10.0.0.0/16"]
+  subnet_prefixes     = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["Sub1", "Sub2", "Sub3", "Sub4"]
+
+  tags = {
+    "Terraform" : "true"
+  }
+
+  depends_on = [azurerm_resource_group.rg]
+}
+
+module "subnet1" {
+  source              = "../modules/subnet1"
+  vnet_subnets        = module.network.vnet_subnets
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+}
