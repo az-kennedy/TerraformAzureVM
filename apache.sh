4111e615990fe6e978e3e6f6f716d7e83042285a
Thomas Kennedy <thomas_kennedy@goodyear.com>
2022-01-22 17:18:38 -0500

    added '-y'

--- apache.sh
+++ apache.sh
@@ -2,6 +2,6 @@
 echo "*** Installing apache"
 sudo yum update -y
 sudo yum update httpd -y
-sudo systemctl start httpd
-sudo systemctl status httpd
+sudo systemctl start httpd -y
+sudo systemctl status httpd -y
 echo "*** Completed Installing apache2"
