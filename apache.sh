a4f12f7f8ec6ce038093a9f50b31a2d81cfbd9f8
Thomas Kennedy <thomas_kennedy@goodyear.com>
2022-01-23 12:21:44 -0500

    added code to disable repo which caused an error

--- apache.sh
+++ apache.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "*** Installing apache"
+sudo subscription-manager repos --disable codeready-builder-for-rhel-8-x86_64-eus-debug-rpms
+sudo dnf clean all
+sudo rm -r /var/cache/dnf
+sudo dnf upgrade
+
 sudo dnf install httpd
 sudo systemctl enable httpd.service
 sudo systemctl start httpd.service
