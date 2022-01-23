2127e67b6553fa2c7a73d5a0899729de46b651e7
Thomas Kennedy <thomas_kennedy@goodyear.com>
2022-01-23 12:30:19 -0500

    updated apache install script

--- apache.sh
+++ apache.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 echo "*** Installing apache"
-sudo subscription-manager repos --disable codeready-builder-for-rhel-8-x86_64-eus-debug-rpms
-sudo dnf clean all
-sudo rm -r /var/cache/dnf
-sudo dnf upgrade
+#sudo subscription-manager repos --disable codeready-builder-for-rhel-8-x86_64-eus-debug-rpms
+#sudo dnf clean all
+#sudo rm -r /var/cache/dnf
+#sudo dnf upgrade
 
-sudo dnf install httpd
-sudo systemctl enable httpd.service
-sudo systemctl start httpd.service
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo systemctl status httpd
 sudo firewall-cmd --zone=public --permanent --add-service=http
 sudo firewall-cmd --reload
 
